f1e9c2e1506e5379d4888a2732c853109cd5f3c6
michelle <caprica@sipswitching.com>
2018-03-05 14:14:18 -0500

    static data dropdown

---
+++ app2.js
@@ -0,0 +1,45 @@
+Ext.require('Ext.container.Viewport');
+Ext.application({
+    name: 'HelloExt',
+    launch: function() {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: [
+                {
+                    title: 'Practice 2',
+                    html : 'Practice Grid 2'
+                }
+            ]
+        });
+    }
+});
+Ext.create('Ext.data.Store', {
+    storeId:'simpsonsStore',
+    fields:['name', 'email', 'phone'],
+    data:{'items':[
+        { 'name': 'Lisa',  "email":"lisa@simpsons.com",  "phone":"555-111-1224"  },
+        { 'name': 'Bart',  "email":"bart@simpsons.com",  "phone":"555-222-1234" },
+        { 'name': 'Homer', "email":"home@simpsons.com",  "phone":"555-222-1244"  },
+        { 'name': 'Marge', "email":"marge@simpsons.com", "phone":"555-222-1254"  }
+    ]},
+    proxy: {
+        type: 'memory',
+        reader: {
+            type: 'json',
+            root: 'items'
+        }
+    }
+});
+
+Ext.create('Ext.grid.Panel', {
+    title: 'Simpsons',
+    store: Ext.data.StoreManager.lookup('simpsonsStore'),
+    columns: [
+        { text: 'Name',  dataIndex: 'name' },
+        { text: 'Email', dataIndex: 'email', flex: 1 },
+        { text: 'Phone', dataIndex: 'phone' }
+    ],
+    height: 200,
+    width: 400,
+    renderTo: Ext.getBody()
+});
